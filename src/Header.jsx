36e0b5e740b9ca67a97d44eb2d87ac295cbb5cf2
sundram <sundramkumar773@gmail.com>
2022-07-06 17:18:41 +0530

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CSST/Header.css"
+
+function Header() {
+    return (
+        <header>
+            <h1>Note App</h1>
+        </header>
+    );
+}
+
+export default Header;
